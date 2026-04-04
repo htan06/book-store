b7097522a8ac5db06ca1ad034c2e946213396d6c
hoangtan <181595064+HoangTan2006@users.noreply.github.com>
2026-04-04 12:07:38 +0700

    refactor: Chia component

---
+++ script/home.js
@@ -0,0 +1,78 @@
+let books = [
+    {
+        "img": "https://cdn1.fahasa.com/media/catalog/product/8/9/8935278605546.jpg",
+        "title": "30 Tuổi - Mọi Thứ Chỉ Mới Bắt Đầu",
+        "price": "100.000 VND"
+    },
+    {
+        "img": "https://cdn1.fahasa.com/media/catalog/product/c/h/chu-nghia-khac-ky-_tb-2023__bia-1.jpg",
+        "title": "Chủ Nghĩa Khắc Kỷ - Phong Cách Sống Bản Lĩnh Và Bình Thản (Tái Bản)",
+        "price": "104.000 đ"
+    },
+    {
+        "img": "https://cdn1.fahasa.com/media/catalog/product/9/7/9786044009674.jpg",
+        "title": "Con Đường Chẳng Mấy Ai Đi",
+        "price": "99.000 đ"
+    },
+    {
+        "img": "https://cdn1.fahasa.com/media/catalog/product/8/9/8936225390492.jpg",
+        "title": "Phương Pháp Đặt Câu Hỏi Của Socrates - Khai Mở Tư Duy Phản Biện Và Hiểu Biết Sâu Sắc",
+        "price": "69.300 đ"
+    },
+    {
+        "img": "https://cdn1.fahasa.com/media/catalog/product/_/2/_2024_-thay_doi_ti_hon_hieu_qua_bat_ngo-tb8-02.jpg",
+        "title": "Atomic Habits - Thay Đổi Tí Hon Hiệu Quả Bất Ngờ (Tái Bản 2023)",
+        "price": "132.000 đ"
+    },
+    {
+        "img": "https://cdn1.fahasa.com/media/catalog/product/l/e/lennui-front_1.jpg",
+        "title": "Lên Đỉnh Núi Mở Tiệm Bánh Mì - Triết Lí Wazawaza - Những Con Người Hạnh Phúc, 2 Loại Bánh Mì, Và Doanh Thu Thường Niên 300 Triệu Yên",
+        "price": "84.000 đ"
+    }
+]
+
+function home() {
+    let html = ''
+    html += banner("./static/img/banner.jpg")
+    html += section("Sách mới", books)
+    html += section("Sách bán chạy", books)
+    return html
+}
+
+function section(title, listCard) {
+    let html = 
+        `<div class="row justify-content-center text-center p-4 bg-light rounded-2 my-2">
+            <h2>${title}</h2>
+            <div class="row">`
+
+    listCard.forEach(e => {
+        html += card(e.img, e.title, e.price)
+    });
+
+    html += 
+        `</div>
+            <button class="btn btn-outline-warning w-25" type="button">Xem thêm</button>
+        </div>`
+    return html
+}
+
+function card(img, title, price) {
+    return `
+        <div class="col-md-2 p-3">
+            <div class="card p-3">
+                <img class="card-img-top"
+                    src="${img}"
+                    alt="">
+                    <p class="card-title text-truncate">${title}</p>
+                    <h6 class="text-danger">${price}</h6>
+                    <a href="#" class="btn btn-warning">Mua ngay</a>
+            </div>
+        </div>`
+}
+
+function banner(img) {
+    return `
+        <div class="row m-3">
+            <img src="${img}">
+        </div>`
+}
---
+++ script/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let main = document.getElementById('main')
+    main.innerHTML += home()
+})
+        
